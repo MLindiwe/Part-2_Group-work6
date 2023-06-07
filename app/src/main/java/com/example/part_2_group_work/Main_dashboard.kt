300ef5cacaf2e8a6b27fcd0fa1b94e63f7942cd9
LiamPillay <st10120651@vcconnect.edu.za>
2023-06-07 17:14:57 +0000

    finished

--- app/src/main/java/com/example/part_2_group_work/Main_dashboard.kt
+++ app/src/main/java/com/example/part_2_group_work/Main_dashboard.kt
@@ -4,24 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
 class Main_dashboard : AppCompatActivity() {
+    private lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_dashboard)
 
+        val Card1: CardView =findViewById(R.id.Card1);
 
+        Card1.setOnClickListener {
+            image = findViewById(R.id.Imguload)
+            UploadImage(image);
+
+
+        }
 
 
 
         val textView10 = findViewById<Button>(R.id.textView10)
-        //create an setonclickListener for that id
+        val viewlist = findViewById<ImageView>(R.id.viewlist)
 
-        //button back
+
+
         textView10.setOnClickListener {
 
             //reference the activity using This@(current activity),login::class.java
-            val intent1 = Intent(this@Main_dashboard, MainActivity::class.java)
+            val intent1 = Intent(this@Main_dashboard, TimesheetEntry::class.java)
+            //call the intent
+            startActivity(intent1)
+
+        }
+
+
+
+        viewlist.setOnClickListener {
+
+            //reference the activity using This@(current activity),login::class.java
+            val intent1 = Intent(this@Main_dashboard, Entrylist::class.java)
             //call the intent
             startActivity(intent1)
 
@@ -36,4 +59,29 @@ class Main_dashboard : AppCompatActivity() {
 
 
     }
+
+
+
+
+
+    private fun UploadImage(image: ImageView){
+        val intent= Intent ()
+        intent.action =Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        startActivityForResult(intent, 1)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1){
+            image.setImageURI(data?.data)
+        }
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/part_2_group_work/TimesheetEntry.kt
+++ app/src/main/java/com/example/part_2_group_work/TimesheetEntry.kt
@@ -1,11 +1,29 @@
 package com.example.part_2_group_work
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TimesheetEntry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timesheet_entry)
+
+
+
+
+        val btnback = findViewById<Button>(R.id.btnback)
+        //create an setonclickListener for that id
+
+        //button back
+        btnback.setOnClickListener {
+
+            //reference the activity using This@(current activity),login::class.java
+            val intent1 = Intent(this@TimesheetEntry, Main_dashboard::class.java)
+            //call the intent
+            startActivity(intent1)
+
+        }
     }
 }
