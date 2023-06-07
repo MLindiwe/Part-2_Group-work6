347e4767feebfdf1801676c3764a98f9802e4668
Alex Kouadio <ST10120876@vcconnect.edu.za>
2023-06-07 10:47:51 +0200

    third

--- app/src/main/java/com/example/part_2_group_work/Main_dashboard.kt
+++ app/src/main/java/com/example/part_2_group_work/Main_dashboard.kt
@@ -1,11 +1,39 @@
 package com.example.part_2_group_work
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Main_dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_dashboard)
+
+
+
+
+
+        val textView10 = findViewById<Button>(R.id.textView10)
+        //create an setonclickListener for that id
+
+        //button back
+        textView10.setOnClickListener {
+
+            //reference the activity using This@(current activity),login::class.java
+            val intent1 = Intent(this@Main_dashboard, MainActivity::class.java)
+            //call the intent
+            startActivity(intent1)
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
